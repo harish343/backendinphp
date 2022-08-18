bcbc79edda9e0bc80d6a77a79ca955d05a9a7106
harish <harishnarnaware75@gmail.com>
2022-08-19 01:19:08 +0530

    added remember me funtionality by cookies

--- registationemail/login.php
+++ registationemail/login.php
@@ -1,6 +1,7 @@
 
 <?php
 session_start();
+ob_start();
 ?>
 
 
@@ -18,12 +19,16 @@ if(isset($_POST['submit'])){
         $_SESSION['username'] = $email_pass['username'];
         $pass_decode = password_verify($password,$db_pass);
         if($pass_decode){
-            echo "login suceesful";
-            ?>
-            <script>
-                location.replace("home.php")
-            </script>
-            <?php
+            if(isset($_POST['rememberme'])){
+                
+                setcookie('emailcookie',$email,time()+86400);
+                setcookie('passcookie',$password,time()+86400);
+                header('location:home.php');
+
+            }
+            else{
+                header('location:home.php');
+            }
         }else{
             echo "password incoreect";
         }
@@ -39,14 +44,16 @@ if(isset($_POST['submit'])){
         <div class="p-2 w-1/2">
             <div class="relative">
                 <label for="email" class="leading-7 text-sm text-gray-600">username</label>
-                <input type="email" id="username" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                <input type="email"  id="username" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" value="<?php if(isset($_COOKIE['emailcookie'])) echo $_COOKIE['emailcookie'] ?>">
             </div>
         </div>
         <div class="p-2 w-1/2">
             <div class="relative">
                 <label for="email" class="leading-7 text-sm text-gray-600">email</label>
-                <input type="password" id="password" name="password" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                <input type="password" id="password" name="password" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out " value="<?php if(isset($_COOKIE['passcookie'])) echo $_COOKIE['passcookie'] ?>">
             </div>
+            <label for="remember me">remember me</label>
+            <input type="checkbox" id="rememberme" name="rememberme" value="remember me">
         </div>
 
 
--- registationemail/logout.php
+++ registationemail/logout.php
@@ -3,6 +3,8 @@
 session_start();
 
 session_destroy();
+setcookie('emailcookie','',time()-86400);
+setcookie('passcookie','',time()-86400);
 
 header('location:login.php');
 
